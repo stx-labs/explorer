6e92020b94e456f08a324b4cfb910fa34f5673f0
nick-stacks <233811846+nick-stacks@users.noreply.github.com>
2025-10-17 11:52:23 -0500

    fix: contract ids can be searched via tx id page (#2551)
    Co-authored-by: nick-stacks <nick-stacks@users.noreply.github.com>

--- src/app/txid/[txId]/page-data.ts
+++ src/app/txid/[txId]/page-data.ts
@@ -1,9 +1,16 @@
 import { stacksAPIFetch } from '@/api/stacksAPIFetch';
 
-import { MempoolTransaction, Transaction } from '@stacks/stacks-blockchain-api-types';
+import {
+  MempoolTransaction,
+  SmartContract,
+  Transaction,
+} from '@stacks/stacks-blockchain-api-types';
 
 export const getTxTag = (txId: string) => `tx-id-${txId}`;
+export const getContractTag = (contractId: string) => `contract-id-${contractId}`;
+
 const CONFIRMED_TX_REVALIDATION_TIMEOUT_IN_SECONDS = 3; // 3 seconds
+const CONFIRMED_CONTRACT_REVALIDATION_TIMEOUT_IN_SECONDS = 3; // 3 seconds
 
 export async function fetchTxById(
   apiUrl: string,
@@ -19,4 +26,20 @@ export async function fetchTxById(
 
   const tx: Transaction | MempoolTransaction = await response.json();
   return tx;
+}
+
+export async function fetchContractById(
+  apiUrl: string,
+  contractId: string
+): Promise<SmartContract> {
+  const response = await stacksAPIFetch(`${apiUrl}/extended/v1/contract/${contractId}`, {
+    cache: 'default',
+    next: {
+      revalidate: CONFIRMED_CONTRACT_REVALIDATION_TIMEOUT_IN_SECONDS,
+      tags: [getContractTag(contractId)],
+    },
+  });
+
+  const contract: SmartContract = await response.json();
+  return contract;
 }
