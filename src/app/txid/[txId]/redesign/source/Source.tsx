4d61c9e06593111cb71866024cb54f9dd5cf1ee6
nick-stacks <nick-stacks@users.noreply.github.com>
2025-10-23 18:11:32 -0500

    fix: removed deployment link if linking to the same page

--- src/app/txid/[txId]/redesign/source/Source.tsx
+++ src/app/txid/[txId]/redesign/source/Source.tsx
@@ -5,6 +5,7 @@ import { useContractById } from '@/common/queries/useContractById';
 import { buildUrl } from '@/common/utils/buildUrl';
 import { ButtonLink } from '@/ui/ButtonLink';
 import { Stack } from '@chakra-ui/react';
+import { usePathname } from 'next/navigation';
 
 import {
   ContractCallTransaction,
@@ -31,15 +32,16 @@ export function Source({
   const { data: txContract } = useContractById(txContractId);
   const sourceCode = txContract?.source_code;
   const network = useGlobalContext().activeNetwork;
+  const url = buildUrl(`/txid/${encodeURIComponent(txContractId)}`, network);
+  const pathname = usePathname();
+  const needLink = !url.includes(pathname);
   return (
     <Stack gap={3}>
-      <ButtonLink
-        href={buildUrl(`/txid/${encodeURIComponent(txContractId)}`, network)}
-        buttonLinkSize="small"
-        aria-label="View deployment"
-      >
-        View deployment{' '}
-      </ButtonLink>
+      {needLink && (
+        <ButtonLink href={url} buttonLinkSize="small" aria-label="View deployment">
+          View deployment{' '}
+        </ButtonLink>
+      )}
       <CodeEditorWithControls code={sourceCode || ''} />
     </Stack>
   );
