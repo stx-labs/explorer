ab3aa169f66aeadc65a5627ea2198b2b1828981a
nick-stacks <233811846+nick-stacks@users.noreply.github.com>
2026-02-10 01:06:47 -0600

    fix: removed deployment link if linking to the same page (#2569)
    Co-authored-by: nick-stacks <nick-stacks@users.noreply.github.com>
    Co-authored-by: heidar-stacks <233772527+heidar-stacks@users.noreply.github.com>

--- src/app/txid/[txId]/redesign/source/Source.tsx
+++ src/app/txid/[txId]/redesign/source/Source.tsx
@@ -5,6 +5,7 @@ import { useContractById } from '@/common/queries/useContractById';
 import { buildUrl } from '@/common/utils/buildUrl';
 import { ButtonLink } from '@/ui/ButtonLink';
 import { Stack } from '@chakra-ui/react';
+import { usePathname } from 'next/navigation';
 
 import { CodeEditor, withControls } from './CodeEditor';
 
@@ -14,15 +15,16 @@ export function Source({ contractId }: { contractId: string }) {
   const { data: txContract } = useContractById(contractId);
   const sourceCode = txContract?.source_code;
   const network = useGlobalContext().activeNetwork;
+  const url = buildUrl(`/txid/${encodeURIComponent(contractId)}`, network);
+  const pathname = usePathname();
+  const needLink = !url.includes(pathname);
   return (
     <Stack gap={3}>
-      <ButtonLink
-        href={buildUrl(`/txid/${encodeURIComponent(contractId)}`, network)}
-        buttonLinkSize="small"
-        aria-label="View deployment"
-      >
-        View deployment{' '}
-      </ButtonLink>
+      {needLink && (
+        <ButtonLink href={url} buttonLinkSize="small" aria-label="View deployment">
+          View deployment{' '}
+        </ButtonLink>
+      )}
       <CodeEditorWithControls code={sourceCode || ''} />
     </Stack>
   );
